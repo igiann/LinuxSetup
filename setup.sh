31205b2beda546f71e9253b209e0d06c4d14373f
Joan Jani <Joan>
2020-02-23 10:23:35 +0100

    Brave installX

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-####! /bin/bash
+#! /bin/bash
 
 #### Graphics
 sudo apt install gimp ink inkscape -y
@@ -8,6 +8,11 @@ sudo apt install vlc -y
 
 #### Browsers
 sudo apt install chromium-browser -y
+sudo apt install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
 
 #### LaTeX
 sudo apt install texlive-latex-extra -y
