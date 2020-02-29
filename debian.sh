2c96f80772755a2f24045b980f317234f08c82b8
Joan Jani <Joan>
2020-02-29 10:21:38 +0100

    Debian xorg xfce4

--- debian.sh
+++ debian.sh
@@ -1,3 +1,19 @@
-
-
+#https://averagelinuxuser.com/10-things-to-do-after-installing-debian/
 apt install sudo
+echo -n "Enter your username: "
+read username
+adduser $username sudo
+apt install xorg
+apt install xfce4
+
+# sudo nano /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade
+sudo reboot
+
+
+#sudo nano /usr/share/lightdm/lightdm.conf.d/01_my.conf
+#[SeatDefaults]
+#greeter-hide-users=false
+
+
