ff69f5f451777166a6a2dc7421ae60e2172e2e89
Joan <igiann@hotmail.com>
2021-03-22 18:17:04 +0100

    Calibre

--- rclone.sh
+++ rclone.sh
@@ -1,3 +1,5 @@
-rclone --vfs-cache-mode writes mount OneDrive: /home/igiann/Documents/rClone/FTI &
+rclone --vfs-cache-mode writes mount OneDriveFIMIF: /home/igiann/Documents/rClone/FIMIF
 
-rclone --vfs-cache-mode writes mount Virtus:   /home/igiann/Documents/rClone/Virtus/ &
+#rclone --vfs-cache-mode writes mount OneDrive: /home/igiann/Documents/rClone/FTI &
+
+#rclone --vfs-cache-mode writes mount Virtus:   /home/igiann/Documents/rClone/Virtus/ &
--- wineguard.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/max-moser/network-manager-wireguard
-cd network-manager-wireguard
-./autogen.sh --without-libnm-glib
-
-./configure --without-libnm-glib --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib/x86_64-linux-gnu --libexecdir=/usr/lib/NetworkManager --localstatedir=/var
-
-make   
-sudo make install
