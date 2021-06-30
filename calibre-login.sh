da5ca8bcda02b7d4095a86486f3e3dc1f42bf96d
Joan <igiann@hotmail.com>
2021-06-30 13:26:21 +0200

    Calibre

--- calibre-login.sh
+++ calibre-login.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -avz --ignore-existing  --delete  /home/igiann/'Calibre Library/'  igiann@192.168.0.100:/media/usb/'Calibre\ Library'
-scp /home/igiann/Calibre\ Library/metadata.db  igiann@192.168.0.100:/media/usb/'Calibre\ Library'
-scp /home/igiann/Calibre\ Library/metadata_db_prefs_backup.json  igiann@192.168.0.100:/media/usb/'Calibre\ Library'
-calibre
+rsync -avz --ignore-existing  --delete igiann@192.168.192.100:/mnt/usb/'Calibre\ Library' /home/igiann/'Calibre Library/'
+scp   igiann@192.168.192.100:/mnt/usb/'Calibre\ Library' /home/igiann/Calibre\ Library/metadata.db
+scp  igiann@192.168.192.100:/mnt/usb/'Calibre\ Library' /home/igiann/Calibre\ Library/metadata_db_prefs_backup.json 
 
