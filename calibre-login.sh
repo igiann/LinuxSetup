6f6cae060edf8f3dd006129e6255658f21985247
Joan <igiann@hotmail.com>
2021-07-01 14:43:23 +0200

    Login is working

--- calibre-login.sh
+++ calibre-login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -avz --ignore-existing  --delete igiann@192.168.192.100:/mnt/usb/'Calibre\ Library' /home/igiann/'Calibre Library/'
-scp   igiann@192.168.192.100:/mnt/usb/'Calibre\ Library' /home/igiann/Calibre\ Library/metadata.db
-scp  igiann@192.168.192.100:/mnt/usb/'Calibre\ Library' /home/igiann/Calibre\ Library/metadata_db_prefs_backup.json 
+rsync -avz --ignore-existing  --delete igiann@192.168.192.100:/mnt/usb/'Calibre\ Library'/ /home/igiann/Calibre\ Library/
+scp   igiann@192.168.192.100:/mnt/usb/'Calibre\ Library'/metadata.db /home/igiann/Calibre\ Library/
+scp  igiann@192.168.192.100:/mnt/usb/'Calibre\ Library'/metadata_db_prefs_backup.json  /home/igiann/Calibre\ Library/
 
