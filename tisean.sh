6d130e8e0a73e6b3fee6b044d08b01d392ff317b
Joan Jani <igiann@hotmail.com>
2021-07-14 12:25:27 +0200

    hello

--- tisean.sh
+++ tisean.sh
@@ -7,10 +7,9 @@ sudo unzip ./TISEAN_3.0.1.zip
 sudo rm TISEAN_3.0.1.zip
 sudo chown igiann:igiann /usr/local/Tisean_3.0.1/ -R
 cd /usr/local/Tisean_3.0.1/
-./configure --prefix=/usr/local/Tisean_3.0.1/bin
+./configure --prefix=/usr/local/Tisean_3.0.1/
 make
 make install
-export PATH=$PATH:/usr/local/Tisean_3.0.1/bin/
-
+export PATH=/usr/local/Tisean_3.0.1/bin:$PATH
 
 
