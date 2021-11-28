394a5367eaaac3fa52ee11bb05d257b54b2bfbc7
Joan <igiann@hotmail.com>
2021-11-28 09:52:22 +0100

    Publishers

--- latex.sh
+++ latex.sh
@@ -9,6 +9,7 @@ sudo apt install texlive-xetex -y
 sudo get install texlive-font-utils -y
 sudo get install texlive-publishers -y
 sudo get install texlive-fonts-extra -y
+sudo apt-get install texlive-publishers
 
 #####  Install mcode
 sudo mkdir /usr/share/texmf/tex/latex/mcode/
