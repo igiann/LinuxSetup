d730e9789888e1bd5c6204882c82d20a2f2b9c19
Joan <igiann@hotmail.com>
2023-03-24 11:14:41 +0100

    adsf

--- calibre-login.sh
+++ calibre-login.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rsync -avz --ignore-existing  --delete igiann@192.168.192.100:/mnt/usb/'Calibre\ Library'/ /home/igiann/Calibre\ Library/
-scp   igiann@192.168.192.100:/mnt/usb/'Calibre\ Library'/metadata.db /home/igiann/Calibre\ Library/
-scp  igiann@192.168.192.100:/mnt/usb/'Calibre\ Library'/metadata_db_prefs_backup.json  /home/igiann/Calibre\ Library/
+rsync -avz --ignore-existing  --delete igiann@192.168.192.100:/mnt/usb/'Calibre Library'/ /home/igiann/Calibre\ Library/
+scp   igiann@192.168.192.100:/mnt/usb/'Calibre Library'/metadata.db /home/igiann/Calibre\ Library/
+scp  igiann@192.168.192.100:/mnt/usb/'Calibre Library'/metadata_db_prefs_backup.json  /home/igiann/Calibre\ Library/
 
