9c127c705fe7b3991f1db31dc1bf85dd2147a235
Joan <igiann@hotmail.com>
2023-10-25 11:24:35 +0200

    pdf_Download

--- tisean.sh
+++ tisean.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-sudo apt install gfortran
+sudo apt-get install fort77
 
 cd /usr/local/
-sudo wget https://www.pks.mpg.de/~tisean/TISEAN_3.0.1.zip
-sudo unzip ./TISEAN_3.0.1.zip
-sudo rm TISEAN_3.0.1.zip
+sudo wget https://www.pks.mpg.de/tisean/TISEAN_3.0.1.tar.gz
+sudo gunzip TISEAN_3.0.1.tar.gz
+sudo rm TISEAN_3.0.1.tar.gz
 sudo chown igiann:igiann /usr/local/Tisean_3.0.1/ -R
 cd /usr/local/Tisean_3.0.1/
-./configure --prefix=/usr/local/Tisean_3.0.1/
+./configure --prefix=/usr/local/Tisean_3.0.1
 make
 make install
 export PATH=/usr/local/Tisean_3.0.1/bin:$PATH
